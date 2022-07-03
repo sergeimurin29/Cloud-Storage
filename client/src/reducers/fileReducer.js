ebfbb7f85ec4c7cf35e11777f619a7545313cf11
Sergey <sergeimurin29@gmail.com>
2022-07-03 18:21:52 +0300

    CORS policy

---
+++ client/src/reducers/fileReducer.js
@@ -0,0 +1,9 @@
+const defaultState = {};
+
+export const fileReducer = (state = defaultState, action) => {
+    switch (action.type) {
+        default: {
+            return state;
+        }
+    }
+}
---
+++ server/middleware/cors.middleware.js
@@ -0,0 +1,9 @@
+const cors = (request, response, next) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    response.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+}
+
+module.exports = cors;
+
