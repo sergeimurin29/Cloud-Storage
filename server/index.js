998be6855876a58075feba6c0627e3fdf79210d3
Sergey <sergeimurin29@gmail.com>
2022-07-03 12:43:02 +0300

    Data base diagram

--- server/index.js
+++ server/index.js
@@ -1,19 +1,23 @@
+const {MongoClient} = require("mongodb");
 const express = require("express");
 const config = require("config");
-const MongoDBUser = require("./MongDBUserConfig.json");
 
-const {MongoClient} = require("mongodb");
-const client = new MongoClient(`mongodb+srv://sergeyDB:${MongoDBUser.dbUserPassword}@cloudstoragecluster.t1un3dc.mongodb.net/?retryWrites=true&w=majority`);
+const app = express();
+const PORT = config.get("serverPort");
 
+const client = new MongoClient(`mongodb+srv://${config.get("dbUser")}:${config.get("dbUserPassword")}@cloudstoragecluster.t1un3dc.mongodb.net/?retryWrites=true&w=majority`);
 const start = async () => {
     try {
         await client.connect();
-        console.log(`Connected to ${MongoDBUser.dbName} data base!`);
-    } catch (e) {
-        console.log(e);
+        console.log(`Connected to ${config.get("dbName")} data base!`);
+
+        app.listen(PORT, () => {
+            console.log("Server started on port", PORT);
+        })
+
+    } catch (error) {
+        console.log(error);
     }
 }
 
 start().then();
-
-/*pGmtIHytgN6bA7pE*/
